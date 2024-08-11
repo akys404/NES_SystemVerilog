04bc59a0ca4d397b707d3e9960594622c001ecc3
akys404 <autumnbetter@gmail.com>
2024-08-11 22:14:23 +0900

    Add VGA function

--- RTL/03_Peripheral/ROM_RAM.sv
+++ RTL/03_Peripheral/ROM_RAM.sv
@@ -23,6 +23,48 @@ endmodule
 
 
 // ==========================================================
+//                 VGA VRAM
+// ==========================================================
+module VRAM #(
+    parameter int DATA_WIDTH = 8,
+    parameter int ADDR_WIDTH = 16
+)(
+    input logic clock_a,
+    input logic wren_a,
+    input logic [ADDR_WIDTH-1:0] address_a,
+    input logic [DATA_WIDTH-1:0] data_a,
+    output logic [DATA_WIDTH-1:0] q_a,
+
+    input logic clock_b,
+    input logic wren_b,
+    input logic [ADDR_WIDTH-1:0] address_b,
+    input logic [DATA_WIDTH-1:0] data_b,
+    output logic [DATA_WIDTH-1:0] q_b
+);
+
+    // Memory block definitions
+    (* ramstyle = "M10K" *) logic [DATA_WIDTH-1:0] ram [0:(2**ADDR_WIDTH)-1];
+
+    // Port A operation
+    always_ff @(posedge clock_a) begin
+        if (wren_a) begin
+            ram[address_a] <= data_a;
+        end
+        q_a <= ram[address_a];
+    end
+
+    // Port B operation
+    always_ff @(posedge clock_b) begin
+        if (wren_b) begin
+            ram[address_b] <= data_b;
+        end
+        q_b <= ram[address_b];
+    end
+
+endmodule
+
+
+// ==========================================================
 //                 VGA CROM
 // ==========================================================
 module CROM(
--- RTL/03_Peripheral/VGA.sv
+++ RTL/03_Peripheral/VGA.sv
@@ -7,65 +7,33 @@ module VGA(
     output logic vga_hsync, vga_vsync,
     output logic [3:0] vga_r, vga_g, vga_b
 );
+// ==========================================================
+//                 Parameter
+// ==========================================================
+parameter HPERIOD = 10'd800;
+parameter HFRONT = 10'd80;
+parameter HWIDTH = 10'd96;
+parameter HBACK = 10'd112;
+
+parameter VPERIOD = 10'd525;
+parameter VFRONT = 10'd10;
+parameter VWIDTH = 10'd2;
+parameter VBACK = 10'd33;
+
+parameter PPU_HDISP = 9'd256;
+parameter PPU_VDISP = 9'd240;
+parameter PPU_DELAY = 9'd3;     // INDEX_BG -> INDEX -> PRAM
+parameter VGA_DELAY = 10'd3;    // VRAM -> CROM -> FF
 
 
 // ==========================================================
-//                 Counter operation
+//                 Module definitions
 // ==========================================================
-logic [9:0] vga_hcnt;
-always_ff @(posedge clk_vga)
-    if (reset)
-        vga_hcnt <= 10'd0;
-    else if (vga_hcnt == 10'd799)
-        vga_hcnt <= 10'd0;
-    else
-        vga_hcnt <= vga_hcnt + 10'd1;
-
-logic [9:0] vga_vcnt;
-always_ff @(posedge clk_vga)
-    if (reset)
-        vga_vcnt <= 10'd0;
-    else if (vga_hcnt == 10'd799 && vga_vcnt == 10'd524)
-        vga_vcnt <= 10'd0;
-    else if (vga_hcnt == 10'd799)
-        vga_vcnt <= vga_vcnt + 10'd1;
-
-
-// ==========================================================
-//                 vga_hsync / vga_vsync
-// ==========================================================
-always_ff @(posedge clk_vga)
-    if (reset)
-        vga_hsync <= 1'b0;
-    else if (vga_hcnt == 10'd594)
-        vga_hsync <= 1'b0;
-    else if (vga_hcnt == 10'd690)
-        vga_hsync <= 1'b1;
-
-always_ff @(posedge clk_vga)
-    if (reset)
-        vga_vsync <= 1'b0;
-    else if (vga_hcnt == 10'd594 && vga_vcnt == 10'd489)
-        vga_vsync <= 1'b0;
-    else if (vga_hcnt == 10'd594 && vga_vcnt == 10'd491)
-        vga_vsync <= 1'b1;
-
-
-// ==========================================================
-//                 VGA VRAM
-// ==========================================================
-// Stores data in VRAM
-wire [5:0] vga_pixel;
-wire [8:0] vram_hcnt = ppu_hcnt -  9'd4;
-wire [15:0] address_a = {ppu_vcnt[7:0], vram_hcnt[7:0]};
-wire [15:0] address_b = {vga_vcnt[8:1], vga_hcnt[8:1]};
-wire vram_we = vram_hcnt <= 9'd255 && ppu_vcnt <= 9'd239;
 VRAM VRAM (
-// Create a 65536 × 8 2-Port RAM using IP cores
     // NES section
     .data_a(ppu_pixel),
     .address_a(address_a),
-    .wren_a(vram_we),
+    .wren_a(wren),
     .clock_a(clk_ppu),
     .q_a(),
     // VGA section
@@ -76,11 +44,6 @@ VRAM VRAM (
     .q_b(vga_pixel)
 );
 
-
-// ==========================================================
-//                 VGA CROM
-// ==========================================================
-wire [11:0] vga_rgb;
 CROM CROM(
     .clk(clk_vga),
     .address(vga_pixel[5:0]),
@@ -89,23 +52,112 @@ CROM CROM(
 
 
 // ==========================================================
-//                 VGA RGB
+//                 Counter operation
 // ==========================================================
-logic [1:0] vga_rgb_en;
-always_ff @(posedge clk_vga)
-    if (vga_hcnt < 512 && vga_vcnt < 480)
-        vga_rgb_en <= {vga_rgb_en[0], 1'b1};
-    else
-        vga_rgb_en <= {vga_rgb_en[0], 1'b0};
+logic [9:0] vga_hcnt;
+logic [9:0] vga_vcnt;
 
-// VGA_R, VGA_G, VGA_B
+wire hcntend = (vga_hcnt == HPERIOD - 10'd1);
+
+always_ff @(posedge clk_vga)
+    if (reset)
+        vga_hcnt <= 10'd0;
+    else if (hcntend)
+        vga_hcnt <= 10'd0;
+    else
+        vga_hcnt <= vga_hcnt + 10'd1;
+
+always_ff @(posedge clk_vga)
+    if (reset)
+        vga_vcnt <= 10'd0;
+    else if (hcntend)
+        if (vga_vcnt == VPERIOD - 10'd1)
+            vga_vcnt <= 10'd0;
+        else
+            vga_vcnt <= vga_vcnt + 10'd1;
+
+
+// ==========================================================
+//                 vga_hsync / vga_vsync
+// ==========================================================
+wire [9:0] hsstart = HFRONT - 10'd1;
+wire [9:0] hsend = HFRONT + HWIDTH - 10'd1;
+wire [9:0] vsstart = VFRONT;
+wire [9:0] vsend = VFRONT + VWIDTH;
+
+always_ff @(posedge clk_vga)
+    if (reset)
+        vga_hsync <= 1'b1;
+    else if (vga_hcnt == hsstart)
+        vga_hsync <= 1'b0;
+    else if (vga_hcnt == hsend)
+        vga_hsync <= 1'b1;
+
+always_ff @(posedge clk_vga)
+    if (reset)
+        vga_vsync <= 1'b1;
+    else if (vga_hcnt == hsstart)
+        if (vga_vcnt == vsstart)
+            vga_vsync <= 1'b0;
+        else if (vga_vcnt == vsend)
+            vga_vsync <= 1'b1;
+
+
+// ==========================================================
+//                 NES section
+// ==========================================================
+wire [8:0] address_a_lsb = ppu_hcnt - PPU_DELAY;
+wire [15:0] address_a = {ppu_vcnt[7:0], address_a_lsb[7:0]};
+wire h_wren = address_a_lsb < PPU_HDISP;
+wire v_wren = ppu_vcnt < PPU_VDISP;
+wire wren =  h_wren && v_wren; 
+
+
+// ==========================================================
+//                 VGA section
+// ==========================================================
+// VRAM section
+logic [5:0] vga_pixel;
+wire [9:0] vga_ihcnt = vga_hcnt - HFRONT - HWIDTH - HBACK + VGA_DELAY;
+wire [9:0] vga_ivcnt = vga_vcnt - VFRONT - VWIDTH - VBACK;
+wire [15:0] address_b = {vga_ivcnt[8:1], vga_ihcnt[8:1]};
+
+// CROM section
+logic [11:0] vga_rgb;
+
+// FF section
+logic hdispen, vdispen;
+
+wire [9:0] hdstart = HFRONT + HWIDTH + HBACK - 10'd2;
+wire [9:0] hdend = HPERIOD - 10'd2;
+wire [9:0] vdstart = VFRONT + VWIDTH + VBACK - 10'd1;
+wire [9:0] vdend = VPERIOD - 10'd1;
+wire dispen = hdispen && vdispen;
+
+always_ff @(posedge clk_vga)
+    if (reset)
+        hdispen <= 1'b0;
+    else if (vga_hcnt == hdstart)
+        hdispen <= 1'b1;
+    else if (vga_hcnt == hdend)
+        hdispen <= 1'b0;
+
+always_ff @(posedge clk_vga)
+    if (reset)
+        vdispen <= 1'b0;
+    else if (vga_hcnt == hdstart)
+        if (vga_vcnt == vdstart)
+            vdispen <= 1'b1;
+        else if (vga_vcnt == vdend)
+            vdispen <= 1'b0;
+
 always_ff @(posedge clk_vga)
     if (reset) begin
         vga_r <= 4'h0;
         vga_g <= 4'h0;
         vga_b <= 4'h0;
     end
-    else if (vga_rgb_en[1]) begin
+    else if (dispen) begin
         vga_r <= vga_rgb[11:8];
         vga_g <= vga_rgb[7:4];
         vga_b <= vga_rgb[3:0];
